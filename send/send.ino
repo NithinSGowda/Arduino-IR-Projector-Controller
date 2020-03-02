8a973cb766f6887bd5a0a5a5eac2c9aa4edb969d
Nithin.S <nithin.s491@gmail.com>
2020-03-02 09:44:31 +0530

    Add files via upload

--- send/send.ino
+++ send/send.ino
@@ -27,15 +27,176 @@ void loop() {
   duration = pulseIn(echoPin,HIGH);
   distance=duration/58.2;
 
+  if(distance > 5 && distance < 10)
+  {
+    digitalWrite(buzzer,HIGH);
+    irsend.sendNEC(0x4CB311EE, 32);
+    Serial.println("Menu");
+    Serial.println(distance);
+    Serial.println("         ");
+    delayMicroseconds(1000);
+  }
+
+  
   if(distance > 10 && distance < 15)
   {
     digitalWrite(buzzer,HIGH);
-    irsend.sendNEC(0x4CB3817E, 32); //Power Code
+    irsend.sendNEC(0x4CB3817E, 32);
+    Serial.println("POWER");
+    Serial.println(distance);
+    Serial.println("         ");
+    delayMicroseconds(1000);
   }
+
+
+  if(distance > 15 && distance < 20)
+  {
+    digitalWrite(buzzer,HIGH);
+    irsend.sendNEC(0x4CB353AC, 32);
+    Serial.println("Ok");
+    Serial.println(distance);
+    Serial.println("         ");
+    delayMicroseconds(1000);
+  }
+
+
+
+  if(distance > 20 && distance < 25)
+  {
+    digitalWrite(buzzer,HIGH);
+    irsend.sendNEC(0x4CB3639C, 32);
+    Serial.println("Up");
+    Serial.println(distance);
+    Serial.println("         ");
+    delayMicroseconds(1000);
+  }
+
+
+
+  if(distance > 25 && distance < 30)
+  {
+    digitalWrite(buzzer,HIGH);
+    irsend.sendNEC(0x4CB3E31C, 32);
+    Serial.println("Down");
+    Serial.println(distance);
+    Serial.println("         ");
+    delayMicroseconds(1000);
+  }
+
+
+
+  if(distance > 30 && distance < 35)
+  {
+    digitalWrite(buzzer,HIGH);
+    irsend.sendNEC(0x4CB3936C, 32);
+    Serial.println("right");
+    Serial.println(distance);
+    Serial.println("         ");
+    delayMicroseconds(1000);
+  }
+
+
+
+  if(distance > 35 && distance < 40)
+  {
+    digitalWrite(buzzer,HIGH);
+    irsend.sendNEC(0x4CB313EC , 32);
+    Serial.println("Left");
+    Serial.println(distance);
+    Serial.println("         ");
+    delayMicroseconds(1000);
+  }
+
+
+
+
+  if(distance > 40 && distance < 45)
+  {
+    digitalWrite(buzzer,HIGH);
+    
+    irsend.sendNEC(0x4CB311EE, 32);
+    delay(300);
+    irsend.sendNEC(0x4CB3936C, 32);
+    delay(300);
+    irsend.sendNEC(0x4CB3936C , 32);
+    delay(300);
+    irsend.sendNEC(0x4CB3E31C , 32);
+    delay(300);
+    irsend.sendNEC(0x4CB3E31C , 32);
+    delay(300);
+    irsend.sendNEC(0x4CB3936C , 32);
+    
+    Serial.println("Brightness");
+    Serial.println(distance);
+    Serial.println("         ");
+    delayMicroseconds(1000);
+  }
+
+
+
+  if(distance > 45 && distance < 50)
+  {
+    digitalWrite(buzzer,HIGH);
+    irsend.sendNEC(0x44CB331CE, 32);
+    Serial.println("Volume Up");
+    Serial.println(distance);
+    Serial.println("         ");
+    delayMicroseconds(1000);
+  }
+
+
+
+  if(distance > 50 && distance < 55)
+  {
+    digitalWrite(buzzer,HIGH);
+    irsend.sendNEC(0x4CB3F10E, 32);
+    Serial.println("Volume Down");
+    Serial.println(distance);
+    Serial.println("         ");
+    delayMicroseconds(1000);
+  }
+
+
+
+  if(distance > 515 && distance < 610)
+  {
+    digitalWrite(buzzer,HIGH);
+    irsend.sendNEC(0x4CB323DC, 32);
+    Serial.println("ReSync");
+    Serial.println(distance);
+    Serial.println("         ");
+    delayMicroseconds(1000);
+  }
+
+  if(distance > 60 && distance < 65)
+  {
+    digitalWrite(buzzer,HIGH);
+    
+    irsend.sendNEC(0x4CB311EE, 32);
+    delay(300);
+    irsend.sendNEC(0x4CB3936C, 32);
+    delay(300);
+    irsend.sendNEC(0x4CB3E31C , 32);
+    delay(300);
+    irsend.sendNEC(0x4CB3E31C , 32);
+    delay(300);
+    irsend.sendNEC(0x4CB3E31C , 32);
+    delay(300);
+    irsend.sendNEC(0x4CB3E31C , 32);
+    delay(300);
+    irsend.sendNEC(0x4CB3936C , 32);
+    
+    Serial.println("Zoom");
+    Serial.println(distance);
+    Serial.println("         ");
+    delayMicroseconds(1000);
+  }
+
+  
   else{
     digitalWrite(buzzer,LOW);
     }
-  Serial.println(distance);
+  //Serial.println(distance);
 
-  delay(50);  
+  delay(1000);  
 }
